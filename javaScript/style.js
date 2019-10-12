3aabffbdd11119c1fc6e3b8d2e30fe29c250c971
Safaa_mujahed <safamojahed123@gmail.com>
2019-10-12 15:50:37 +0300

    change the form tags

---
+++ javaScript/style.js
@@ -0,0 +1,8 @@
+let member = new Object();
+member = {
+    name: document.getElementById('name'),
+    email: document.getElementById('email'),
+    major: document.getElementById('major'),
+    role: document.getElementById('roel'),
+    biography: document.getElementById('biography')
+}
