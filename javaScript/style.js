6181c8ce953cbff325c8fca5a96aebdc4b8f1ae1
Safaa_mujahed <safamojahed123@gmail.com>
2019-10-12 20:50:48 +0300

    add the object to the local storage

--- javaScript/style.js
+++ javaScript/style.js
@@ -1,8 +1,26 @@
-let member = new Object();
-member = {
-    name: document.getElementById('name'),
-    email: document.getElementById('email'),
-    major: document.getElementById('major'),
-    role: document.getElementById('roel'),
-    biography: document.getElementById('biography')
+ let save = document.getElementById('save');
+let members = new Array();
+
+function createMember(name, email, major, role, biography) {
+    this.name = name;
+    this.email = email;
+    this.major = major;
+    this.role = role;
+    this.biography = biography;
 }
+
+save.addEventListener("click", function(e) {
+    let memeberName = document.getElementById("name").value;
+    let memberEmail = document.getElementById("email").value;
+    let memberMajor = document.getElementById("major").value;
+    let memberRole = document.getElementById("role").value;
+    let memberBiography = document.getElementById("biography").value;
+    let member = new createMember(memeberName, memberEmail, memberMajor, memberRole, memberBiography);
+    members.push(member);
+    localStorage.setItem('teamMembers', JSON.stringify(members));
+    let allMembers = new Array();
+    allMembers = localStorage.getItem('teamMembers') ?
+    JSON.parse(localStorage.getItem('teamMembers')) : []
+});
+
+
