f744ea206c99b8382d6b24154514d7cbe51d585b
Safaa_mujahed <safamojahed123@gmail.com>
2019-10-14 22:16:53 +0300

    format code

--- javaScript/style.js
+++ javaScript/style.js
@@ -4,21 +4,20 @@ let cancel = document.getElementById('cancel-bourd-button');
 let deleteItem = document.getElementById('delete-bourd-button');
 let close = document.getElementById('close-bourd');
 members = localStorage.getItem('teamMembers') ?
-JSON.parse(localStorage.getItem('teamMembers')) : []
+    JSON.parse(localStorage.getItem('teamMembers')) : []
 
 function displayItems() {
     document.getElementById("card").innerHTML = "";
     members.forEach(element => {
-    document.getElementById("card").innerHTML += displayCard(element.name,
-    element.email, element.major, element.role, element.biography);
+        document.getElementById("card").innerHTML += displayCard(element.name,
+            element.email, element.major, element.role, element.biography);
     });
 }
 
 displayItems();
 displayNoOfItems();
 
-function createMember(name, email, major, role, biography) 
-{
+function createMember(name, email, major, role, biography) {
     this.name = name;
     this.email = email;
     this.major = major;
@@ -38,50 +37,46 @@ save.addEventListener("click", function(e) {
     let member = new createMember(memeberName, memberEmail, memberMajor, memberRole, memberBiography);
     let valid = required(memeberName, memberEmail, memberMajor, memberRole, memberBiography);
     let unique = emailIsUnique(memberEmail);
-    if(valid && unique && !addBottom && index == "") {
-            members.push(member);
-            localStorage.setItem('teamMembers', JSON.stringify(members));
-            displayItems();
-            displayNoOfItems();
-        }
-    else 
-        if(valid && unique && index == "") {
-                members.unshift(member);
-                localStorage.setItem('teamMembers', JSON.stringify(members));
-                displayItems();
-                displayNoOfItems();
-            }
-    else 
-        if(valid && unique && !addBottom ) {
-                members.splice(index, 0, member);
-                localStorage.setItem('teamMembers', JSON.stringify(members));
-                displayItems();
-                displayNoOfItems();
-            }
+    if (valid && unique && !addBottom && index == "") {
+        members.push(member);
+        localStorage.setItem('teamMembers', JSON.stringify(members));
+        displayItems();
+        displayNoOfItems();
+    } else
+    if (valid && unique && index == "") {
+        members.unshift(member);
+        localStorage.setItem('teamMembers', JSON.stringify(members));
+        displayItems();
+        displayNoOfItems();
+    } else
+    if (valid && unique && !addBottom) {
+        members.splice(index, 0, member);
+        localStorage.setItem('teamMembers', JSON.stringify(members));
+        displayItems();
+        displayNoOfItems();
+    }
     console.log(members);
 });
 
-function required(name, email, major, role, biography) 
-{
-  if (name.length === 0 || email.length === 0 || major.length === 0 ||
-    role.length === 0 || biography.length === 0)
-   { 
-      alert("All fields are required please enter all the required fields.");  	
-      return false; 
-   }  	
-   return true; 
-} 
+function required(name, email, major, role, biography) {
+    if (name.length === 0 || email.length === 0 || major.length === 0 ||
+        role.length === 0 || biography.length === 0) {
+        alert("All fields are required please enter all the required fields.");
+        return false;
+    }
+    return true;
+}
 
-function emailIsUnique (enteredEmail){
-    let validEmail = members.find( member => member.email === enteredEmail);
-    if(validEmail !== undefined) {
+function emailIsUnique(enteredEmail) {
+    let validEmail = members.find(member => member.email === enteredEmail);
+    if (validEmail !== undefined) {
         alert("This email address is already exists")
         return false;
     }
     return true;
- }
+}
 
- function displayCard(name, email, major, role, biography ) {
+function displayCard(name, email, major, role, biography) {
     return ` <div class="rightContentCards">
         <div class="deleteIcon">
             <img src="images/delete-icon.png" alt="delete">
@@ -92,15 +87,15 @@ function emailIsUnique (enteredEmail){
             <p> ${biography} </p>
         </div>
         </div>`
- }
+}
 
- function displayNoOfItems() {
+function displayNoOfItems() {
     document.getElementById("noOfItems").innerHTML = "";
-     let noOfItems = `<p>${members.length} ITEMS</p>`
+    let noOfItems = `<p>${members.length} ITEMS</p>`
     document.getElementById("noOfItems").innerHTML = noOfItems;
- } 
+}
 
- item.addEventListener("click", function() {
+item.addEventListener("click", function() {
     let name = this.name;
     let email = this.email;
     let major = this.major;
@@ -110,18 +105,18 @@ function emailIsUnique (enteredEmail){
     document.getElementById("secound-artbourd").style.display = "inline";
 });
 
-function displayItem(name, email, major, role, biography ) {
+function displayItem(name, email, major, role, biography) {
     document.getElementById("member-name").innerHTML = name;
     document.getElementById("member-deatails").innerHTML = email;
     document.getElementById("member-major").innerHTML = major;
     document.getElementById("member-role").innerHTML = role;
-    document.getElementById("member-biography").innerHTML = biography;    
- }
+    document.getElementById("member-biography").innerHTML = biography;
+}
 
-cancel.addEventListener("click",function() {
+cancel.addEventListener("click", function() {
     document.getElementById("secound-artbourd").style.display = "none";
 })
 
-close.addEventListener("click",function() {
+close.addEventListener("click", function() {
     document.getElementById("secound-artbourd").style.display = "none";
 })
