9a35b1363d3272c49f7119c09725c99078a17093
Safaa_mujahed <safamojahed123@gmail.com>
2019-10-15 18:33:45 +0300

    delete button is ready

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,76 +1,85 @@
-document.addEventListener("DOMContentLoaded",function(e)
-{
+document.addEventListener("DOMContentLoaded", function(e) {
     let save = document.getElementById('save');
     let cancel = document.getElementById('cancel-board-button');
-    let deleteItem = document.getElementById('delete-bourd-button');
+    let delItem = document.getElementById("delete-board-button");
     let close = document.getElementById('close-artboard');
+
     /* if statement to check if we have a local storage or not, so if it's exists,
       we will equal members array to it after parse it to objects, if not then members will be empty. */
     members = localStorage.getItem('teamMembers') ?
         JSON.parse(localStorage.getItem('teamMembers')) : []
-    
+
     //this function display all the members stored in the array in the right content of the page.
-   
     displayItems();
     displayNoOfItems();
-    
-    
-//add eventListener on save button.
-save.addEventListener("click", function(e) {
-    //after the user enter the values for all fields, they will be saved in variablies.
-    let memeberName = document.getElementById("name").value;
-    let memberEmail = document.getElementById("email").value;
-    let memberMajor = document.getElementById("major").value;
-    let memberRole = document.getElementById("role").value;
-    let memberBiography = document.getElementById("biography").value;
-    //take the resualt of the checkbox and index input.
-     let addBottom = document.getElementById("add-botton").checked;
-    let index = document.getElementById("index").value;
-    //after save the values in variablies, these values will be used to create new object.
-    let member = new createMember(memeberName, memberEmail, memberMajor, memberRole, memberBiography);
-    //call function required to check if all fieldl are not empty.
-    let valid = required(memeberName, memberEmail, memberMajor, memberRole, memberBiography);
-    //call function emailIdUnique to check if the email address if unique.
-    let unique = emailIsUnique(memberEmail);
-    if (valid && unique && !addBottom && index == "") {
-        //if the user dosn't choose the checkbox or index the object wil be added by default at top.
-        //first I push the element to members array and then I parse the array to string 
-        //and and then add it to localstorage.
-        members.push(member);
-        localStorage.setItem('teamMembers', JSON.stringify(members));
-        displayItems();
-        displayNoOfItems();
-    } else
-    if (valid && unique && index == "") {
-        //if checkbox value is true, then the object will be added at the bottom of the local storage 
-        //by using unshift function.
-        members.unshift(member);
-        localStorage.setItem('teamMembers', JSON.stringify(members));
-        displayItems();
-        displayNoOfItems();
-    } else
-    if (valid && unique && !addBottom) {
-        //if user choose index field, then the object will be added to local storage at the specified index.
-        members.splice(index, 0, member);
-        localStorage.setItem('teamMembers', JSON.stringify(members));
-        displayItems();
-        displayNoOfItems();
-    }
-});
-//this function will close the second artboard if the user click on x icon.
-cancel.addEventListener("click", function() {
-    document.getElementById("second-artboard").style.display = "none";
-})
 
-//this function will close the second artboard if the user clidk on cancel button.
-close.addEventListener("click", function() {
-    document.getElementById("second-artboard").style.display = "none";
-})
+    //add eventListener on save button.
+    save.addEventListener("click", function(e) {
+        //after the user enter the values for all fields, they will be saved in variablies.
+        let memeberName = document.getElementById("name").value;
+        let memberEmail = document.getElementById("email").value;
+        let memberMajor = document.getElementById("major").value;
+        let memberRole = document.getElementById("role").value;
+        let memberBiography = document.getElementById("biography").value;
+        //take the resualt of the checkbox and index input.
+        let addBottom = document.getElementById("add-botton").checked;
+        let index = document.getElementById("index").value;
+        //after save the values in variablies, these values will be used to create new object.
+        let member = new createMember(memeberName, memberEmail, memberMajor, memberRole, memberBiography);
+        //call function required to check if all fieldl are not empty.
+        let valid = required(memeberName, memberEmail, memberMajor, memberRole, memberBiography);
+        //call function emailIdUnique to check if the email address if unique.
+        let unique = emailIsUnique(memberEmail);
+        if (valid && unique && !addBottom && index == "") {
+            //if the user dosn't choose the checkbox or index the object wil be added by default at top.
+            //first I push the element to members array and then I parse the array to string 
+            //and and then add it to localstorage.
+            members.push(member);
+            localStorage.setItem('teamMembers', JSON.stringify(members));
+            displayItems();
+            displayNoOfItems();
+        } else
+        if (valid && unique && index == "") {
+            //if checkbox value is true, then the object will be added at the bottom of the local storage 
+            //by using unshift function.
+            members.unshift(member);
+            localStorage.setItem('teamMembers', JSON.stringify(members));
+            displayItems();
+            displayNoOfItems();
+        } else
+        if (valid && unique && !addBottom) {
+            //if user choose index field, then the object will be added to local storage at the specified index.
+            members.splice(index, 0, member);
+            localStorage.setItem('teamMembers', JSON.stringify(members));
+            displayItems();
+            displayNoOfItems();
+        }
+    });
+
+    //this function will close the second artboard if the user click on x icon.
+    cancel.addEventListener("click", function() {
+        document.getElementById("second-artboard").style.display = "none";
+    });
+
+    delItem.addEventListener("click", function() {
+        let email = document.getElementById("member-deatails").innerHTML;
+        let member = members.find(member => member.email === email);
+        members.shift(member);
+        localStorage.setItem('teamMembers', JSON.stringify(members));
+        displayItems();
+        displayNoOfItems();
+    });
+    
+    //this function will close the second artboard if the user clidk on cancel button.
+    close.addEventListener("click", function() {
+        document.getElementById("second-artboard").style.display = "none";
+    })
 });
+
 function displayItems() {
     document.getElementById("card").innerHTML = "";
     members.forEach(element => {
-        document.getElementById("card").innerHTML += displayCard(element.name, element.email, element.major, element.role, element.biography);            
+        document.getElementById("card").innerHTML += displayCard(element.name, element.email, element.major, element.role, element.biography);
     });
 }
 
@@ -111,7 +120,6 @@ function emailIsUnique(enteredEmail) {
 
 //this function will display all the members  in the right content of the page.
 function displayCard(name, email, major, role, biography) {
-    console.log(email);
     return ` <div class="right-content-cards" id="${email}">
         <div class="delete-icon">
             <img src="images/delete-icon.png" alt="delete" onclick="deleteItem('${email}')">
@@ -142,7 +150,6 @@ function displayItem(name, email, major, role, biography) {
 
 //this function delete the memeber, if the user click on delete icon.
 function deleteItem(email) {
-    console.log("dasf");
     let member = members.find(member => member.email === email);
     members.shift(member);
     localStorage.setItem('teamMembers', JSON.stringify(members));
@@ -153,12 +160,7 @@ function deleteItem(email) {
 //display the second artboard for every member.
 function displayPop(email) {
     let member = members.find(member => member.email === email);
-    console.log(member);
     displayItem(member.name, member.email, member.major, member.role, member.biography);
     document.getElementById("second-artboard").style.display = "inline";
 };
-
-// deleteItem.addEventListener("click", functioin() {
-//  console.log("hi");
-// });
 
